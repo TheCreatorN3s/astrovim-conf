b41a83a1531c3e97aa9098556d4927f095766826
n3s <n3s.creator@gmail.com>
2026-02-26 03:21:37 -0400

    add: astrovim-conf

---
+++ lua/community.lua
@@ -0,0 +1,27 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- lenguage support packs [LSP, tree-sitter, DAP]
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.java" },
+  -- import/override with your plugins folder
+  -- colorschemes
+
+  -- moves packages
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.editing-support.vim-move" },
+
+  --ui plugins
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+}
